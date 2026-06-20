b9f1c68797b8d3151c16d053ce6b440047acf493
DarthSidM <mauryasiddharth14@gmail.com>
2026-06-20 18:14:50 +0000

    added home / and completed dockerfile

--- engine/app/main.py
+++ engine/app/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
-
+import uvicorn
 from app.routes.resume import router as resume_router
+from app.routes.home import router as home_router
+
 
 app = FastAPI()
 
@@ -8,3 +10,9 @@ app.include_router(
     resume_router,
     prefix="/api/v2"
 )
+app.include_router(
+    home_router,
+    prefix=""
+)
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=5000)
