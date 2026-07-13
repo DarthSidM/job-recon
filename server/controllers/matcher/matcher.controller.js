7a32827b613e72a2ce19518691888f7e9b82d0b3
DarthSidM <mauryasiddharth14@gmail.com>
2026-07-13 06:33:35 +0000

    added redis for job cache

--- server/controllers/matcher/matcher.controller.js
+++ server/controllers/matcher/matcher.controller.js
@@ -1,9 +1,10 @@
 import { getActiveResume } from "../../repos/resume.repo.js";  
+import redis from "../../lib/redis.js"
 import axios from "axios"
 import dotenv from "dotenv"
 dotenv.config();
 
-
+const CACHE_TTL=60*60;
 export async function getMatchedJobsController(req, res){
     const { id } = req.user;
     try{
@@ -14,9 +15,24 @@ export async function getMatchedJobsController(req, res){
                 message: "No active resume found",
             });
         }
+        const cacheKey = `matched_jobs:${resume.id}`;
+        //cache hit
+        const cachedJobs = await redis.get(cacheKey);
+        if (cachedJobs) {
+            return res.status(200).json({
+                status: "success",
+                message: "jobs (cached)",
+                jobData: JSON.parse(cachedJobs),
+            });
+        }
+        //cache miss
         const { data } = await axios.get(
             `${process.env.ENGINE_URL}/matcher/${resume.id}`
         );
+        //get data then store in redis for 1 hr
+        await redis.set(cacheKey, JSON.stringify(data), {
+            EX: CACHE_TTL,
+        });
 
         return res.status(200).json({
             status: "success",
